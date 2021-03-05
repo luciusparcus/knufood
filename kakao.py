6bf16290a2ef983dea20a3fa24ace5147c0a7733
Lucas Park <75164959+luciusparcus@users.noreply.github.com>
2021-03-05 17:59:38 +0900

    Change the order of creating replies

--- kakao.py
+++ kakao.py
@@ -5,6 +5,7 @@ from menu import Menu, DormMenu
 
 app = Flask(__name__)
 
+
 def create_reply(text):
     return jsonify({
         "version": "2.0",
@@ -18,6 +19,7 @@ def create_reply(text):
             ]
         }
     })
+
 
 @app.route("/knufood", methods=["POST"])
 def knufood():
@@ -35,9 +37,10 @@ def knufood():
     # Set the default reply as unknown
     command = data["userRequest"]["utterance"]
 
+    if command in (u"감꽃푸드코트", u"공학관교직원식당", u"공학관학생식당", u"복지관", u"복현카페테리아", u"정보센터식당", u"카페테리아첨성"):
+        print(create_reply(Menu(command).show()))
+        return create_reply(Menu(command).show())
     if command == u"누리관":
         return create_reply(DormMenu(command).show())
-    if command in (u"감꽃푸드코트", u"공학관교직원식당", u"공학관학생식당", u"복지관", u"복현카페테리아", u"정보센터식당", u"카페테리아첨성"):
-        return create_reply(Menu(command).show())
 
     return create_reply("지원되지 않는 기능입니다.")
