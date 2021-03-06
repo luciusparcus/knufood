66b8a2f05d6a57173912a99b1c6d3309bf7109f3
Lucas Park <75164959+luciusparcus@users.noreply.github.com>
2021-03-06 10:27:08 +0900

    Remove signal

--- kakao.py
+++ kakao.py
@@ -56,24 +56,3 @@ def knufood():
     return create_reply(menus[command])
 
 
-import signal,sys
-def receiveSignal(signum,frame):
-    print("you chose to end the program by {}".format(signum))
-    sys.exit()
-
-
-signal.signal(signal.SIGHUP, receiveSignal)
-signal.signal(signal.SIGINT, receiveSignal)
-signal.signal(signal.SIGQUIT, receiveSignal)
-signal.signal(signal.SIGILL, receiveSignal)
-signal.signal(signal.SIGTRAP, receiveSignal)
-signal.signal(signal.SIGABRT, receiveSignal)
-signal.signal(signal.SIGBUS, receiveSignal)
-signal.signal(signal.SIGFPE, receiveSignal)
-signal.signal(signal.SIGKILL, receiveSignal)
-signal.signal(signal.SIGUSR1, receiveSignal)
-signal.signal(signal.SIGSEGV, receiveSignal)
-signal.signal(signal.SIGUSR2, receiveSignal)
-signal.signal(signal.SIGPIPE, receiveSignal)
-signal.signal(signal.SIGALRM, receiveSignal)
-signal.signal(signal.SIGTERM, receiveSignal)
