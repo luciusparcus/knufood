71634bb19c69d3aab4fe7f9c82d48c43fb0f6d65
Lucas Park <75164959+luciusparcus@users.noreply.github.com>
2021-03-06 17:23:27 +0900

    Change indexing order

--- menu.py
+++ menu.py
@@ -146,7 +146,7 @@ class Menu:
 점심: {}
 
 저녁: {}""".format(self.name, get_day(weekday), get_weekday(weekday),
-                 self.data[weekday][0], self.data[weekday][1], self.data[weekday][2])
+                 self.data[0][weekday], self.data[1][weekday], self.datax[2][weekday])
 
     def is_expired(self):
         return self.date.date() < datetime.now().date()
