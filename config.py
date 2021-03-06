16ae319c991da957ae15ab0aac4a4fc595b90c49
Lucas Park <75164959+luciusparcus@users.noreply.github.com>
2021-03-06 12:11:20 +0900

    Check heroku dyno env in config.py

--- config.py
+++ config.py
@@ -4,8 +4,9 @@ import json
 import os
 
 # Import configuration
-with open("config.json") as f:
-    data = json.load(f)
+if not os.getenv("DYNO", None):
+    with open("config.json") as f:
+        data = json.load(f)
 
 # Configure BOT_ID
 bot_id = os.getenv("BOT_ID") if os.getenv("BOT_ID", None) else data["botId"]
