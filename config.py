e1f750bba0036ff9663900a30ae676e000c5f786
Lucas Park <75164959+luciusparcus@users.noreply.github.com>
2021-03-06 12:23:03 +0900

    Add a colon in config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 
 # Import configuration
 if os.getenv("DYNO", None):
-    if os.getenv("BOT_ID", None)
+    if os.getenv("BOT_ID", None):
         bot_id = os.getenv("BOT_ID")
 else:
     with open("config.json") as f:
